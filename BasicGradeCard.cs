f8dea1ccf2efef5093eedecaf1e6f87d63e8b09c
Anjali-st <anjali90908080@gmail.com>
2023-08-03 02:37:43 +0530

    Polymorephism

---
+++ BasicGradeCard.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsApp_Polymorephism
+{
+    public class BasicGradeCard
+
+    {
+
+        protected int totalMarks;
+
+
+
+        public BasicGradeCard()
+
+        {
+
+            totalMarks = 0;
+
+        }
+
+
+
+        public virtual int getTotalMarks()
+
+        {
+
+            return totalMarks;
+
+        }
+
+    }
+
+} 
+
